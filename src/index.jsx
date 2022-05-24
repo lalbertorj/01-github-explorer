d9f5451eeaed642563b612904b972de7f20aa375
Luiz Alberto <lalbertorj@gmail.com>
2022-05-23 23:36:42 -0300

    ReactJS Chapter 1 [ RocketSeat - Ignite ]

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+/*import React from "react";  --Para importar de forma global basta definir a propriedada Babel present-react = automatic*/
+import { render } from "react-dom";
+import { App } from "./App";
+
+render(<App/>, document.getElementById('root'))
